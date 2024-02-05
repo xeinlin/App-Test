57e534f7a77eb4de8c85765704b45e605a196fd2
Hein Lin <heinlin.me@gmail.com>
2024-02-05 17:53:00 +0630

    Life Cycle

--- app/src/main/java/com/example/apptest/MainActivity.kt
+++ app/src/main/java/com/example/apptest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.apptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.apptest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.tvGreeting.text = "Hi"
 
+        Log.d("TAG","onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG","onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("TAG","onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAG","onDestroy")
     }
 }
